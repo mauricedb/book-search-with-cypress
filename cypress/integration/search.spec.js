1d6e70760d810d68984233ae8895f5c08d240038
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-09-22 19:26:54 +0200

    Intercepting Requests

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -13,4 +13,28 @@ describe('Search', () => {
       'be.visible'
     );
   });
+
+  it('intercept with a StringMatcher', () => {
+    cy.intercept('/books/v1/volumes?*').as('book-search');
+
+    cy.get('select').select('inauthor');
+    cy.get('input').type('Douglas Adams{enter}');
+
+    cy.contains('.card-title', "The Hitchhiker's Guide to the Galaxy").should(
+      'be.visible'
+    );
+  });
+
+  it('intercept with a RouteMatcher', () => {
+    cy.intercept({ pathname: '/books/v1/volumes', method: 'get' }).as(
+      'book-search'
+    );
+
+    cy.get('select').select('inauthor');
+    cy.get('input').type('Douglas Adams{enter}');
+
+    cy.contains('.card-title', "The Hitchhiker's Guide to the Galaxy").should(
+      'be.visible'
+    );
+  });
 });
