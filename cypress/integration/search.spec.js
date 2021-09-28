ba28a88ddb1bde15ffa9c953e57b8003310ed892
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-09-28 18:54:43 +0200

    Intercepting the response

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -61,4 +61,27 @@ describe('Search', () => {
 
     cy.contains('.card-title', 'Book two').should('be.visible');
   });
+
+  it('intercept and use the response', () => {
+    cy.intercept('/books/v1/volumes?*').as('book-search');
+
+    cy.get('select').select('inauthor');
+    cy.get('input').type('Douglas Adams{enter}');
+
+    cy.wait('@book-search').then((xhr) => {
+      const books = xhr.response.body.items;
+
+      books.forEach((book, index) => {
+        cy.get('.card')
+          .eq(index)
+          .within(() => {
+            cy.get('.card-title').should('have.text', book.volumeInfo.title);
+            cy.get('.card-author').should(
+              'have.text',
+              book.volumeInfo.authors.join(', ')
+            );
+          });
+      });
+    });
+  });
 });
