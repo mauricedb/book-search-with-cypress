35c56122955db050d0c1e4de5bb8956e3e3b211d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-12-09 09:58:53 +0100

    React Router Dom V6

--- cypress/integration/request.spec.js
+++ cypress/integration/request.spec.js
@@ -19,7 +19,7 @@ describe('Using cy.request()', () => {
     ).as('response');
 
     cy.get('@response').its('body.kind').should('equal', 'books#volumes');
-    cy.get('@response').its('body.totalItems').should('be.greaterThan', 350);
+    cy.get('@response').its('body.totalItems').should('be.greaterThan', 300);
     cy.get('@response').its('body.items').should('have.length', 25);
     cy.get('@response').its('body.items.0.volumeInfo.title').should('exist');
     cy.get('@response').its('body.items.0.volumeInfo.authors').should('exist');
